16d7fe19eb38dc913d988e9d3bd8576670017f8f
Elizabeth White <redfragle88@gmail.com>
2023-02-17 09:07:29 -0500

    exercise upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+
+// var can be declared multiple times in the same scope, but let can only be declared once in the same scope.This is because let is block scoped, and var is function scoped. Vars are declared before the code is run and let is declared when the execution reaches the line of code.
+
+// const is a constant, and cannot be changed once declared and is also block scoped.
+
+// Vars are hoisted to the top of the function, and let and const are not hoisted.
